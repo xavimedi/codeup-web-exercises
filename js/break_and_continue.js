1f920938883a0ed2e0a60710b6f65e3c22a83d86
Xavier <xavier.g.medina3@gmail.com>
2023-04-20 10:51:35 -0500

    put numberSkip in a function so I can call and test faster

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,23 @@
 "use strict"
 
-let skip;
-    while (true){
+
+
+function numberSkip() {
+    let skip;
+    while (true) {
         skip = prompt("Please enter an odd number between 1 and 50");
-        if (skip>= 1 && skip <= 50 && skip% 2 !== 0) {
-        break;
+        if (skip >= 1 && skip <= 50 && skip % 2 !== 0) {
+            break;
+        }
     }
-}
 
-console.log("Number to skip is:" + skip);
+    console.log("Number to skip is:" + skip);
 
-for (let i = 1; i <= 50; i += 2) {
-    if (i === parseInt(userInput)) {
-        console.log("Yikes! Skipping number: " + skip);
-        continue;
+    for (let i = 1; i <= 50; i += 2) {
+        if (i === parseInt(skip)) {
+            console.log("Yikes! Skipping number: " + skip);
+            continue;
+        }
+        console.log("Here's another odd number: " + i);
     }
-    console.log("Here's another odd number: " + i);
 }
