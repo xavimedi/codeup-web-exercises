fe803f3ef3a07bb103dbad43d04e1bc491a4aecd
Xavier <xavier.g.medina3@gmail.com>
2023-04-25 09:18:46 -0500

    planets - added log

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,23 +19,30 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets.unshift("The Sun"));
+    planets.unshift("The Sun");
+    console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets.push("Pluto"));
+    console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets.shift());
+    console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets.pop());
+    console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
     console.log(planets.indexOf("Earth"));
+    console.log(planets);
 
     console.log("Reversing the order of the planets array.");
     console.log(planets.reverse());
+    console.log(planets);
 
     console.log("Sorting the planets array.");
     console.log(planets.sort());
+    console.log(planets);
 })();
